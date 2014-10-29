8b511b33ddce9f1fc7e4b9b60ac4397ae7478901
paulofer1 <paulo.r.r.fernandes>
2014-10-29 22:39:11 +0000

    V0.2

--- DefectDetection.cpp
+++ DefectDetection.cpp
@@ -148,18 +148,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	int thresh = 100;
 	int max_thresh = 255;
 	RNG rng(12345);
-	src = imread("img.jpg", 1);
+/*	src = imread("img.jpg", 1);
 	cvtColor(src, src_gray, CV_BGR2GRAY);
 	blur(src_gray, src_gray, Size(3, 3));
 	Canny(src_gray, canny_output, thresh, thresh * 2, 3);
 	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+
 	for (int i = 0; i< contours.size(); i++)
 	{
 		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
 	}
-
+	*/
 	while (1)
 	{	
 		//Frame *frame = camera[0]->GetFrame();
